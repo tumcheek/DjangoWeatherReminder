184c91fbb02256e2b64375eb630750e9dce95617
Danil Tumanov <dania.tumanov@gmail.com>
2022-11-27 10:27:06 +0200

    create models

--- django_weather_reminder/main/models.py
+++ django_weather_reminder/main/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class UserModel(AbstractUser):
+    username = None
+    first_name = None
+    last_name = None
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+class CityModel(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class PeriodModel(models.Model):
+    period_of_notice = models.IntegerField()
+
+
+class SubscribersModel(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    city = models.ForeignKey(CityModel, on_delete=models.CASCADE)
+    period = models.ForeignKey(PeriodModel, on_delete=models.CASCADE)
