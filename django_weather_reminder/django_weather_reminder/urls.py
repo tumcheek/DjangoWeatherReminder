5f2c60512e87109d56c0bcb72d998bb9076ed3f7
Danil Tumanov <dania.tumanov@gmail.com>
2022-12-02 17:35:59 +0200

    add path for app

--- django_weather_reminder/django_weather_reminder/urls.py
+++ django_weather_reminder/django_weather_reminder/urls.py
@@ -22,6 +22,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/weather/', include('main.urls')),
     path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
