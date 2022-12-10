3160bd481a7b69d8eb706a5d43fea2c269422691
Danil Tumanov <dania.tumanov@gmail.com>
2022-12-10 12:25:32 +0200

    creat views for app

--- django_weather_reminder/main/views.py
+++ django_weather_reminder/main/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import *
+from .serializers import *
+from .utils import *
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = UserModel.objects.all()
+    serializer_class = UserSerializer
+
+
+class SubscribersViewSet(viewsets.ModelViewSet):
+    serializer_class = SubscribersSerializer
+    queryset = SubscribersModel.objects.all()
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    serializer_class = CitySerializer
+    queryset = CityModel.objects.all()
+
+
+class PeriodViewSet(viewsets.ModelViewSet):
+    serializer_class = PeriodSerializer
+    queryset = PeriodModel.objects.all()
+
+
+class CitiAPIView(APIView):
+    def get(self, request, city_name):
+        coords = get_city_coordinates(city_name)
+        weather_info = get_city_weather(coords['lat'], coords['lon'])
+        return Response(weather_info)
