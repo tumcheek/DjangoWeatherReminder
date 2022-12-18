0e6c3c3d86844a6b37b2731f435c38c8a7fa8303
Danil Tumanov <dania.tumanov@gmail.com>
2022-12-18 12:50:03 +0200

    add all models to admin

--- django_weather_reminder/main/admin.py
+++ django_weather_reminder/main/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import UserModel
+from .models import *
 
 # Register your models here.
 admin.site.register(UserModel)
+admin.site.register(CityModel)
+admin.site.register(CountryModel)
+admin.site.register(PeriodModel)
+admin.site.register(SubscribersModel)
