c628e702d7466d1e58b4f581ce4c5e0bc6c33d2f
Danil Tumanov <dania.tumanov@gmail.com>
2022-12-18 13:00:41 +0200

    update functions

--- django_weather_reminder/main/utils.py
+++ django_weather_reminder/main/utils.py
@@ -2,11 +2,15 @@ import requests
 from django.conf import settings
 
 WEATHER_API_KEY = getattr(settings, 'WEATHER_API_KEY')
+GEO_URL = getattr(settings, 'GEO_URL')
+WEATHER_URL = getattr(settings, 'WEATHER_URL')
 
 
-def get_city_coordinates(city):
+def get_city_coordinates(city, country):
+    city = city.replace(" ", "&")
+    country = country.replace(" ", "&")
     req = requests.get(
-        f'https://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid={WEATHER_API_KEY}')
+        f'{GEO_URL}q={city,country}&limit=5&appid={WEATHER_API_KEY}')
     coordinates_dict = {
         'lat': req.json()[0]['lat'],
         'lon': req.json()[0]['lon'],
@@ -16,8 +20,9 @@ def get_city_coordinates(city):
 
 
 def get_city_weather(lat, lon):
+    units = 'units=metric'
     req = requests.get(
-        f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}&units=metric'
+        f'{WEATHER_URL}lat={lat}&lon={lon}&appid={WEATHER_API_KEY}&{units}'
     )
     return req.json()
 
