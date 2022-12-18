cc7355c55719df73900614a7d14aeb4dffd8d8e8
Danil Tumanov <dania.tumanov@gmail.com>
2022-12-18 12:47:18 +0200

    add CountryModel and add __str__method to other models

--- django_weather_reminder/main/models.py
+++ django_weather_reminder/main/models.py
@@ -18,12 +18,29 @@ class UserModel(AbstractUser):
 class CityModel(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+
+class CountryModel(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 class PeriodModel(models.Model):
     period_of_notice = models.IntegerField()
+
+    def __str__(self):
+        return str(self.period_of_notice)
 
 
 class SubscribersModel(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     city = models.ForeignKey(CityModel, on_delete=models.CASCADE)
+    country = models.ForeignKey(CountryModel, on_delete=models.CASCADE)
     period = models.ForeignKey(PeriodModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user)
