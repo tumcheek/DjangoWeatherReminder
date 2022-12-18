32ac7e65d9157a4e23eef41fadcbfbc125c3c53a
Danil Tumanov <dania.tumanov@gmail.com>
2022-12-18 15:21:33 +0200

    add countries router

--- django_weather_reminder/main/urls.py
+++ django_weather_reminder/main/urls.py
@@ -8,11 +8,7 @@ router = routers.SimpleRouter()
 router.register(r'users', UserViewSet)
 router.register(r'subscriptions', SubscribersViewSet)
 router.register(r'cities', CityViewSet)
+router.register(r'countries', CountryViewSet)
 router.register(r'periods', PeriodViewSet)
 
-urlpatterns = [
-
-    path('cities/<str:city_name>/weather-info', CitiAPIView.as_view(), name='city_info'),
-
-]
-urlpatterns += router.urls
+urlpatterns = router.urls
