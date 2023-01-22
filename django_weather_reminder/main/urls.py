9f1748f0bf5438bb80ca60e889eba2fa4c42bc4b
Danil Tumanov <dania.tumanov@gmail.com>
2023-01-22 13:29:28 +0200

    update urls

--- django_weather_reminder/main/urls.py
+++ django_weather_reminder/main/urls.py
@@ -6,9 +6,12 @@ from rest_framework import routers
 app_name = 'main'
 router = routers.SimpleRouter()
 router.register(r'users', UserViewSet)
-router.register(r'subscriptions', SubscribersViewSet)
 router.register(r'cities', CityViewSet)
 router.register(r'countries', CountryViewSet)
 router.register(r'periods', PeriodViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('subscriptions/<int:pk>/', SubscriptionAPIView.as_view(), name='subscriptions-detail'),
+    path('subscriptions/', SubscriptionAPIView.as_view(), name='subscriptions-list'),
+]
+urlpatterns += router.urls
