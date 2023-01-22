4186a31306866c3657e99bb70e4c033eeb79453b
Danil Tumanov <dania.tumanov@gmail.com>
2023-01-22 13:31:10 +0200

    set up for tests

--- django_weather_reminder/django_weather_reminder/celery.py
+++ django_weather_reminder/django_weather_reminder/celery.py
@@ -1,5 +1,5 @@
 import os
-
+from django.conf import settings
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
@@ -15,7 +15,8 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-
+if settings.DEBUG:
+    app.conf.update(task_always_eager=True)
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
