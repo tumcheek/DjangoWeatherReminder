f95588823e75a0bb12339bf76b88a8c232651cba
Danil Tumanov <dania.tumanov@gmail.com>
2023-01-24 13:42:42 +0200

    change develop settings to prod

--- django_weather_reminder/django_weather_reminder/wsgi.py
+++ django_weather_reminder/django_weather_reminder/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_weather_reminder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_weather_reminder.settings.prod')
 
 application = get_wsgi_application()
