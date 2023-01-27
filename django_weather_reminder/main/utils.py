20881894c31e457cafcc8f179316c021b25ed132
Danil Tumanov <dania.tumanov@gmail.com>
2023-01-27 12:35:31 +0200

    update get_city_coordinates and get_city_weather

--- django_weather_reminder/main/utils.py
+++ django_weather_reminder/main/utils.py
@@ -6,24 +6,29 @@ GEO_URL = getattr(settings, 'GEO_URL')
 WEATHER_URL = getattr(settings, 'WEATHER_URL')
 
 
-def get_city_coordinates(city, country):
+def get_city_coordinates(city: str, country: str) -> dict:
     city = city.replace(" ", "&")
     country = country.replace(" ", "&")
     req = requests.get(
         f'{GEO_URL}q={city,country}&limit=5&appid={WEATHER_API_KEY}')
-    coordinates_dict = {
-        'lat': req.json()[0]['lat'],
-        'lon': req.json()[0]['lon'],
-    }
+    req.raise_for_status()
+    try:
+        coordinates_dict = {
+            'lat': req.json()[0]['lat'],
+            'lon': req.json()[0]['lon'],
+        }
+    except IndexError:
+        raise IndexError('You input incorrect city or country')
 
     return coordinates_dict
 
 
-def get_city_weather(lat, lon):
+def get_city_weather(lat: float, lon: float) -> dict:
     units = 'units=metric'
     req = requests.get(
         f'{WEATHER_URL}lat={lat}&lon={lon}&appid={WEATHER_API_KEY}&{units}'
     )
+    req.raise_for_status()
     return req.json()
 
 
@@ -35,6 +40,3 @@ def create_message(weather_info, city, country):
                   f'Max temperature {weather_info["main"]["temp_max"]} \n'
     wind = f'Wind speed {weather_info["wind"]["speed"]}\n'
     return location + descriptions + temperature + wind
-
-
-
